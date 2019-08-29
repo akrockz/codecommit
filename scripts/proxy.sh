af6d63114461a30c55382a807285a0c229fff0ad
akrockz <ak.lm10barca@gmail.com>
2019-08-29 20:24:28 +0800

    Delete proxy.sh

--- scripts/proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Setting ABC proxy env vars..."
-export http_proxy="http://abc.com:80"
-export HTTP_PROXY=$http_proxy
-export no_proxy=abc.com
-export https_proxy=$http_proxy
-export HTTPS_PROXY=$http_proxy
-export NO_PROXY=$no_proxy
